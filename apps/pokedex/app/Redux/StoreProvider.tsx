b8037c9ce9b8fdceac1ab3d1e50e4eedfb1816dc
Rupesh Naik <145644307+rupeshnaik0209@users.noreply.github.com>
2023-12-12 06:03:18 +0000

    removed not needed code

--- apps/pokedex/app/Redux/StoreProvider.tsx
+++ apps/pokedex/app/Redux/StoreProvider.tsx
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 type Props = { children: ReactNode };
-console.log("store", store);
 
 export const StoreProvider: FC<Props> = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
