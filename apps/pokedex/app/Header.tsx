609e505620a515af3aa2c5652320f6b5448c5b7b
Rupesh Naik <145644307+rupeshnaik0209@users.noreply.github.com>
2023-12-13 02:26:37 +0000

    refactored code

--- apps/pokedex/app/Header.tsx
+++ apps/pokedex/app/Header.tsx
@@ -2,18 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Header() {
   return (
-    <nav className="bg-gray-600 flex justify-between items-center h-20 p-4">
+    <nav className="nav">
       <Image
         src="/pokemon.svg"
         width={100}
         height={100}
         alt="Picture of the author"
       />
-      <p className="text-yellow-50 flex justify-center items-center underline">
+      <p className="link">
         <Link href="/">Home</Link>
       </p>
-      <link rel="apple-touch-icon" href="/pokemon.svg" sizes="any" />
-      {/* <img src="../public/pokemon.svg" className="w-40 h-20" /> */}
     </nav>
   );
 }
--- apps/pokedex/app/PokemonDetail/[id]/page.tsx
+++ apps/pokedex/app/PokemonDetail/[id]/page.tsx
@@ -33,25 +33,17 @@ const PokemonDetail = ({ params }: { params: { id: number } }) => {
     return <div suppressHydrationWarning={true}></div>;
   }
   return (
-    <div className="flex flex-col content-center justify-self-center items-center mt-16 gap-y-2 text-cyan-50 decoration-solid decoration-8">
-      <div className="flex flex-col grow place-content-between bg-rose-700 w-8/12 h-24 p-4 rounded-md">
+    <div className="detail_container">
+      <div className="detail_card">
         <PokoCard pokeMon={pokemon} />
         <h3>Height : {pokemon.height}</h3>
         No of moves :- {noOfMoves(pokemon.moves)}
       </div>
-      <div className="bg-white m-4 p-4">
-        <h2 className="text-white-700 font-mono decoration-solid decoration-4">
-          LIST OF MOVES -
-        </h2>
-        <ul
-          className="grid grid-cols-4 grid-rows-3 gap-4"
-          style={{ listStyleType: "none" }}
-        >
+      <div>
+        <h2 className="detail_heading">LIST OF MOVES -</h2>
+        <ul className="detail_grid">
           {pokemon.moves.map((move: move) => (
-            <li
-              key={move.move.name}
-              className="flex bg-rose-700 text-cyan-50 mt-4 w-40 h-10 items-center self-center place-content-center capitalize hover:uppercase rounded-md"
-            >
+            <li key={move.move.name} className="detail_main">
               {move.move.name}
             </li>
           ))}
